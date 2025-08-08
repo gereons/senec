2707a560e09d7f503813973d04856d40d911448d
Gereon Steffens <github@steffens.org>
2025-08-08 17:38:28 +0200

    add wb control methods

--- lib/senec/cloud/connection.rb
+++ lib/senec/cloud/connection.rb
@@ -92,6 +92,15 @@ module Senec
         get "#{MEASUREMENTS_HOST}/measurements/api/v1/systems/#{system_id}/data-availability/timespan?#{params}"
       end
 
+      def lock_wallbox(system_id, wallbox_id, locked)
+        patch "#{WALLBOX_HOST}/v1/systems/#{system_id}/wallboxes/#{wallbox_id}/locked/#{locked}"
+      end
+
+      def set_wallbox(system_id, wallbox_id, setting, body)
+        lock_wallbox(system_id, wallbox_id, false)
+        post "#{WALLBOX_HOST}/v1/systems/#{system_id}/wallboxes/#{wallbox_id}/settings/#{setting}", body
+      end
+
       private
 
       attr_accessor :oauth_token
@@ -235,7 +244,21 @@ module Senec
         # simplecov:enable
       end
 
-      def post(url, data)
+      def patch(url, default: nil)
+        ensure_token_valid!
+
+        response = oauth_token.patch(url)
+        return default unless response.status == 200
+
+        JSON.parse(response.body)
+      rescue StandardError => e
+        # :nocov:
+        warn "API error: #{e.message}"
+        default
+        # :nocov:
+      end
+
+      def post(url, data, default: nil)
         ensure_token_valid!
 
         response = oauth_token.post(
